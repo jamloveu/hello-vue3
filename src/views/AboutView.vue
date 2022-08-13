d59cf8a6048e963bb16f3e3afa6204d5b2d57222
jam <jamis404@163.com>
2022-08-13 16:29:12 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+ console.log(1234567);
+ const state = reactive({
+  count: 7
+ })
+ const onAdd = ()=>{
+  state.count ++ 
+ }
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page {{ state.count }}</h1>
+    <button @click="onAdd">+</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
